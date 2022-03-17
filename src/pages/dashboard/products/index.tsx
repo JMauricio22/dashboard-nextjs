2f6e6f4abd1a9afc5f98a12175deb16d8b0c4699
Mauricio Lemus <rodeznomauricio@gmail.com>
2022-03-17 14:10:00 -0600

    Changing page paths

--- src/pages/dashboard/products/index.tsx
+++ src/pages/dashboard/products/index.tsx
@@ -206,7 +206,7 @@ export default function Products() {
                           </span>
                         </td>
                         <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
-                          <Link href={`/dashboard/edit/${product.id}`}>
+                          <Link href={`/dashboard/products/edit/${product.id}`}>
                             <a className="text-indigo-600 hover:text-indigo-900">Edit</a>
                           </Link>
                         </td>
